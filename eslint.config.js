7fabd3c03a21afc7c76345bd4a33835bd5217551
DungLe <tuandungsonhoapy@gmail.com>
2024-09-21 01:37:02 +0700

    update project

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+
+const reactValue = 'react'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,6 +19,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      [reactValue]: react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +27,35 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'react/prop-types': 0,
+      'react/display-name': 0,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: ['@mui/*/*/*'],
+        },
+      ],
+      'no-console': 1,
+      'no-lonely-if': 1,
+      'no-unused-vars': 1,
+      'no-trailing-spaces': 1,
+      'no-multi-spaces': 1,
+      'no-multiple-empty-lines': 1,
+      'space-before-blocks': ['error', 'always'],
+      'object-curly-spacing': [1, 'always'],
+      indent: ['warn', 2],
+      semi: [1, 'never'],
+      quotes: ['error', 'single'],
+      'array-bracket-spacing': 1,
+      'linebreak-style': 0,
+      'no-unexpected-multiline': 'warn',
+      'keyword-spacing': 1,
+      'comma-dangle': 1,
+      'comma-spacing': 1,
+      'arrow-spacing': 1,
     },
-  },
+  }
 )
