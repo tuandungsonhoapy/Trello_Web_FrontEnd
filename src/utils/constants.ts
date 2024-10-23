d257cdd4e551ea48c3187db90c425d4d80e91f74
DungDev <tuandungsonhoapy@gmail.com>
2024-10-23 16:13:13 +0700

    update to deploy

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,11 @@
 export const boardId = '67132df2f05aebf2893b58ba'
 
-export const API_ROOT = 'https://trello-web-backend-xjnq.onrender.com/api/v1'
+let apiRoot = ''
+if (import.meta.env.DEV) {
+  apiRoot = 'http://localhost:8080/api/v1'
+}
+if (import.meta.env.PROD) {
+  apiRoot = 'https://trello-web-backend-xjnq.onrender.com/api/v1'
+}
+
+export const API_ROOT = apiRoot
